9f823ae0540e0a5044c9593fcb79e52f1f29065b
Davide Caffagni <davidecaffagni98@gmail.com>
2021-12-15 17:52:17 +0100

    removed unused parameteres

--- models/custom-transformer.py
+++ models/custom-transformer.py
@@ -31,9 +31,7 @@ def custom_multi_head_attn(
     query: Tensor,                  # (N, TL, D)
     key: Tensor,                    # (N, SL, D)
     value: Tensor,                  # (N, SL, D)
-    in_proj_weight: Tensor,         # (2 * D, D)
-    in_proj_bias: Tensor,           # (2 * D, ),
-    out_proj_weight: Tensor,        # (HD, D)
+    out_proj_weight: Tensor = None,        # (HD, D)
     out_proj_bias: Tensor = None,          # (D, )
     nhead: int = 1,
     mask: Tensor = None,                   # (N, TL, SL)
@@ -45,15 +43,12 @@ def custom_multi_head_attn(
     assert embd_dim % nhead == 0, "Embedding dimension must be divisible for the number of heads"
     head_dim = embd_dim // nhead
     k, v = key, value
-    # k, v  = _in_projection_packed(key, value, in_proj_weight, in_proj_bias)
     q = query.contiguous().view(tgt_len, bsz * nhead, head_dim).transpose(0, 1)
     k = k.contiguous().view(src_len, bsz * nhead, head_dim).transpose(0, 1)
     v = v.contiguous().view(src_len, bsz * nhead, head_dim).transpose(0, 1)
 
     attn = torch.bmm(q, k.transpose(-2, -1))
     if mask is not None:
-        if nhead > 1:
-            mask = torch.repeat_interleave(mask, repeats=nhead, dim=0).unsqueeze(1)
         attn = attn + mask
     attn = softmax(attn, dim=-1)
     if not training:
@@ -62,7 +57,8 @@ def custom_multi_head_attn(
         attn = dropout(attn, p=dropout_p)
     out = torch.bmm(attn, v)
     out = out.transpose(0, 1).contiguous().view(bsz, tgt_len, embd_dim)
-    out = linear(out, out_proj_weight, out_proj_bias)
+    if nhead > 1:
+        out = linear(out, out_proj_weight, out_proj_bias)
     return out, attn
 
 
@@ -73,25 +69,23 @@ class CustomMHA(nn.Module):
         self. embd_dim = embd_dim
         self.nhead = nhead
         self.dropout_p = dropout_p
-        self.in_proj_weight = nn.parameter.Parameter(torch.empty((2 * embd_dim, embd_dim)))
-        self.in_proj_bias = nn.parameter.Parameter(torch.zeros((2 * embd_dim, )))
-        self.out_proj_weight = nn.parameter.Parameter(torch.empty((embd_dim, embd_dim)))
-        self.out_proj_bias = nn.parameter.Parameter(torch.zeros((embd_dim, )))
         self.key_proj = nn.Linear(embd_dim, embd_dim)
         self.value_proj = nn.Linear(embd_dim, embd_dim)
 
-        nn.init.xavier_uniform_(self.in_proj_weight)
-        nn.init.xavier_uniform_(self.out_proj_weight)
-
-    def forward(self, query: Tensor, key: Tensor, value: Tensor, need_weights: bool = True, attn_mask: Optional[Tensor] = None, *args, **kwargs):
-        k, v = self.key_proj(key), self.value_proj(value)
-        out, attn = custom_multi_head_attn(query, k, v, self.in_proj_weight, self.in_proj_bias,
-         self.out_proj_weight, self.out_proj_bias, self.nhead, attn_mask,
-            self.dropout_p, self.training) 
-        if need_weights:
-            return out, attn
+        if nhead > 1:
+            self.out_proj_weight = nn.parameter.Parameter(torch.empty((embd_dim, embd_dim)))
+            self.out_proj_bias = nn.parameter.Parameter(torch.zeros((embd_dim, )))
+            nn.init.xavier_uniform_(self.in_proj_weight)
+            nn.init.xavier_uniform_(self.out_proj_weight)
         else:
-            return out
+            self.out_proj_weight = None
+            self.out_proj_bias = None
+
+    def forward(self, query: Tensor, key: Tensor, value: Tensor, attn_mask: Optional[Tensor] = None, *args, **kwargs):
+        k, v = self.key_proj(key), self.value_proj(value)
+        out, attn = custom_multi_head_attn(query, k, v, self.out_proj_weight, self.out_proj_bias, self.nhead, attn_mask,
+            self.dropout_p, self.training) 
+        return out, attn
 
 
 class TSPCustomEncoderLayer(nn.Module):
@@ -121,7 +115,6 @@ class TSPCustomEncoder(nn.Module):
 
         self.layers = nn.ModuleList([
             TSPCustomEncoderLayer(d_model, nhead, dim_feedforward, dropout_p, activation, layer_norm_eps, norm_first) for _ in range(layers)])
-        self.norm = LayerNorm(d_model)
     
     def forward(self, query: Tensor, src: Tensor, attn_mask: Tensor = None):
         output, attn_weight = src, None
@@ -129,7 +122,6 @@ class TSPCustomEncoder(nn.Module):
         for mod in self.layers:
             output, attn_weight = mod(query, output, attn_mask)
         
-        output = self.norm(output)
         return output, attn_weight
 
 
